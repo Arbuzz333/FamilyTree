3e39e22d067c7d6d7233d4ec54cc8f04a9eb49ec
Arbuzz333 <arbuz333@hotmail.com>
2021-07-27 20:43:17 +0300

    New reactive module

---
+++ reactiver2dbc/src/test/kotlin/com/reactiver2dbc/avahidov/ReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reactiver2dbc.avahidov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
